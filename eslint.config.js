8b76ee1cd4d464247548b60f63526c363f49535f
jinyongp <dev.jinyongp@gmail.com>
2025-04-29 23:49:35 +0900

    eslint 설정 정리

--- eslint.config.js
+++ eslint.config.js
@@ -2,18 +2,35 @@ import js from '@eslint/js'
 import ts from 'typescript-eslint'
 import globals from 'globals'
 import react from 'eslint-plugin-react'
-import json from '@eslint/json'
+import json from 'eslint-plugin-jsonc'
 import markdown from '@eslint/markdown'
 import stylistic from '@stylistic/eslint-plugin'
-import { defineConfig } from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
+  globalIgnores(['**/node_modules/**', '**/dist/**', '**/build/**']),
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'] },
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   ts.configs.recommended,
   react.configs.flat.recommended,
   stylistic.configs.recommended,
-  { files: ['**/*.json'], plugins: { json }, language: 'json/json', extends: ['json/recommended'] },
-  { files: ['**/*.jsonc'], plugins: { json }, language: 'json/jsonc', extends: ['json/recommended'] },
-  { files: ['**/*.md'], plugins: { markdown }, language: 'markdown/gfm', extends: ['markdown/recommended'] },
+  json.configs['flat/recommended-with-jsonc'],
+  {
+    files: ['**/*.md'],
+    plugins: { markdown },
+    language: 'markdown/gfm',
+    extends: ['markdown/recommended'],
+  },
+  {
+    files: ['**/*.{jsx,tsx}'],
+    rules: {
+      'react/react-in-jsx-scope': ['off'],
+    },
+  },
+  {
+    files: ['**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': ['off'],
+    },
+  },
 ])
